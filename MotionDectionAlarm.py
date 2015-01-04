6bf3e67aee5c70a7bf938975816dae64770bdef5
Fraham <frahamp@gmail.com>
2015-01-04 00:27:36 +0000

    Fix code

--- MotionDectionAlarm.py
+++ MotionDectionAlarm.py
@@ -35,26 +35,27 @@ try:
 	while True :
 		# Read PIR state
 		Current_State = GPIO.input(PinPIR)
-		if Current_State==1 and Previous_State==0:
-		# PIR is triggered
-		print " Motion detected!"
-		# Flash lights and sound buzzer
-		for x in range(0,3):
 		
-			GPIO.output(PinBuzzer,	GPIO.HIGH)
-			GPIO.output(PinRedLED,	GPIO.HIGH)
-			time.sleep(0.5)
+		if Current_State==1 and Previous_State==0:
+			# PIR is triggered
+			print " Motion detected!"
+			# Flash lights and sound buzzer
+			for x in range(0,3):
 			
-			GPIO.output(PinRedLED,	GPIO.LOW)
-			GPIO.output(PinBlueLED,	GPIO.HIGH)
-			time.sleep(0.5)
-			
-			GPIO.output(PinBlueLED,	GPIO.LOW)
-			GPIO.output(PinBuzzer,	GPIO.LOW)
-			time.sleep(0.5)
-			
-			# Record previous state
-			Previous_State=1
+				GPIO.output(PinBuzzer,	GPIO.HIGH)
+				GPIO.output(PinRedLED,	GPIO.HIGH)
+				time.sleep(0.5)
+				
+				GPIO.output(PinRedLED,	GPIO.LOW)
+				GPIO.output(PinBlueLED,	GPIO.HIGH)
+				time.sleep(0.5)
+				
+				GPIO.output(PinBlueLED,	GPIO.LOW)
+				GPIO.output(PinBuzzer,	GPIO.LOW)
+				time.sleep(0.5)
+				
+				# Record previous state
+				Previous_State=1
 		
 		elif Current_State==0 and Previous_State==1:
 			# PIR has returned to ready state
