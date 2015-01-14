02eb602f9d308d7eb515b4694d616a29ddf5b728
Fraham <frahamp@gmail.com>
2015-01-14 16:47:15 +0000

    Added something

--- MotionDectionAlarm.py
+++ MotionDectionAlarm.py
@@ -29,39 +29,39 @@ try:
 	# Loop until PIR output is 0
 	while GPIO.input(PinPIR) == 1:
 		Current_State = 0
-		
+
 	print "Ready"
 	# Loop until users quits with CTRL - C
 	while True :
 		# Read PIR state
 		Current_State = GPIO.input(PinPIR)
-		
+
 		if Current_State==1 and Previous_State==0:
 			# PIR is triggered
 			print " Motion detected!"
 			# Flash lights and sound buzzer
 			for x in range(0, 20):
-			
+
 				#GPIO.output(PinBuzzer,	GPIO.HIGH)
 				GPIO.output(PinRedLED,	GPIO.HIGH)
 				time.sleep(0.1)
-				
+
 				GPIO.output(PinRedLED,	GPIO.LOW)
 				GPIO.output(PinBlueLED,	GPIO.HIGH)
 				time.sleep(0.1)
-				
+
 				GPIO.output(PinBlueLED,	GPIO.LOW)
 				#GPIO.output(PinBuzzer,	GPIO.LOW)
 				time.sleep(0.1)
-				
+
 				# Record previous state
 				Previous_State=1
-		
+
 		elif Current_State==0 and Previous_State==1:
 			# PIR has returned to ready state
 			print " Ready"
 			Previous_State=0
-		
+
 		# Wait for 10 milliseconds
 		time.sleep(0.01)
 except KeyboardInterrupt:
