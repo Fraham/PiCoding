2e98cff8031071f8e0fc5708d75f0b8b62f353d4
Fraham <frahamp@gmail.com>
2015-08-06 18:05:17 +0100

    fix

--- temp.py
+++ temp.py
@@ -9,9 +9,10 @@ base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
+db
+cur
+
 def startUp():
-
-
     db = MySQLdb.connect(host="easyLiving.ml", user="root",passwd="cheeseBurger", db="easyliving")
     cur = db.cursor()
 
